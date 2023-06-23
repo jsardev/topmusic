4a0584d6c9c4b4f398b6721cf47728458e77041f
Jakub Sarnowski <hello@jsarnowski.com>
2023-06-23 13:14:20 +0200

    feat: configure and integrate unocss

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import Routing from "@/pages";
 
-import "./index.css";
+import "virtual:uno.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,8 @@
-import { useState } from "react";
-
-import "./index.css"
-
 const HomePage = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1>Hello World</h1>
+    </div>
   );
 };
 
